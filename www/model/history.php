ac20f3569fbd9b0627865813963eca35c427d3c4
taiga <de9cth27@gmail.com>
2020-08-21 14:07:37 +0900

    html/history.phpの実装

---
+++ www/model/history.php
@@ -0,0 +1,25 @@
+<?php 
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'db.php';
+
+ function get_history($db,$user_id = null){
+    $arg = [];
+    $sql = '
+    SELECT
+        history.history_id,
+        user_id,
+        create_datetime,
+        sum(price * amount) as sum
+    FROM
+        history INNER JOIN  purchase_detail
+    ON
+        history.history_id = purchase_detail.history_id';
+    if($user_id !== null){
+        $sql .= ' where user_id = ? ';
+        $arg[] = $user_id;
+    }
+    $sql .= ' GROUP BY
+        history.history_id 
+    ';
+    return execute_query($db,$sql,$arg);
+}
---
+++ www/view/history_view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <?php include VIEW_PATH . 'templates/head.php'; ?>
+  <title>購入履歴</title>
+  <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'data.css'); ?>">
+</head>
+<body>
+  <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
+  <h1>購入履歴</h1>
+  <div class="container">
+    <?php include VIEW_PATH . 'templates/messages.php'; ?>
+    
+    <?php if(count($data) > 0){ ?>
+      <table class="table table-bordered">
+        <thead class="thead-light">
+          <tr>
+            <th>注文番号</th>
+            <th>購入日時</th>
+            <th>合計金額</th>
+            <th>購入明細</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($data as $data){ ?>
+          <tr>
+            <td><?php print h($data['history_id']); ?></td>
+            <td><?php print h($data['create_datetime']); ?></td>
+            <td><?php print(number_format($data['sum'])); ?>円</td>
+            <td>
+            </td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    <?php } else { ?>
+      <p>購入履歴はありません。</p>
+    <?php } ?> 
+  </div>
+</body>
+</html>
